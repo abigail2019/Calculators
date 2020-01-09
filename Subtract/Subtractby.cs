11e9cc0173d9524f8235e14ea4e4a264909b7fd5
abigail <abigailnankamba@gmail.com>
2020-01-09 16:47:48 +0200

    Feature: Added an Interface

---
+++ Subtract/Subtractby.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Operation;
+namespace Subtract
+{
+   
+        public interface ISubtractby
+        {
+            public dynamic Subtracted(dynamic a, dynamic b);
+        }
+        public class Subtractby : ISubtractby
+        {
+            public dynamic results;
+            public dynamic Subtracted(dynamic a, dynamic b)
+            {
+                results = Subtraction.Subtract(a, b);
+                return results;
+            }
+        }
+}
