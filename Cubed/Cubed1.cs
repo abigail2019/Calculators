7daf5398c33486fe89e1165fe1874bfb6f54c5d8
abigail <abigailnankamba@gmail.com>
2020-01-09 16:39:20 +0200

    Feature: Added an Interface

---
+++ Cubed/Cubed1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Operation;
+namespace Cubed
+{
+    public interface ICubed1
+    {
+        public dynamic Cubed2(dynamic a);
+    }
+    public class Cubed1 : ICubed1
+    {
+        public dynamic result;
+        public dynamic Cubed2(dynamic a)
+        {
+            result = Cube.Cubed(a);
+            return result;
+        }
+
+    }
+}
