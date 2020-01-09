8846753a0dcf79c679f87b4af50ac8b96b6e6b06
abigail <abigailnankamba@gmail.com>
2020-01-09 16:45:20 +0200

    Feature: Added an Interface

---
+++ Square/Squared1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Operation;
+namespace Square1
+{
+    public interface ISquared
+    {
+        public dynamic Squarea(dynamic a);
+    }
+    public class Squared1 : ISquared
+    {
+        public dynamic result;
+        public dynamic Squarea(dynamic a)
+        {
+            result = Square.Squared(a);
+            return result;
+        }
+    }
+}
