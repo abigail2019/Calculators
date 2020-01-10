fa37af3e078be968a222c1a914dfc52e448c85d8
abigail <abigailnankamba@gmail.com>
2020-01-10 10:24:44 +0200

    Feature: Added a Statistics Calculator

---
+++ StatisticsCalculator/StatCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StatOperations;
+using Calculator04;
+using StaticMean;
+namespace StatisticsCalculator
+{
+    public class StatCalculator : Calculator, IStatMean
+    {
+        StatMean mean = new StatMean();
+        public dynamic Mean(dynamic values)
+        {
+            return mean.Mean(values);
+    }
+
+    }
+}
