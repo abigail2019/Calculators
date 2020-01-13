b72389bb697bdc2a3b083dc4a08d7f4006a7d287
abigail <abigailnankamba@gmail.com>
2020-01-13 10:31:40 +0200

    Task: Created Standard Deviation

---
+++ StandardDeviation/StandardDiv.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StandardDeviation
+{
+    public interface IStandardDiv
+    {
+        public dynamic StandardD(dynamic d);
+    }
+    public class StandardDiv:IStandardDiv
+    {
+        public dynamic result;
+        public dynamic StandardD(dynamic d)
+        {
+            result = StatOperations.StatOperations.StandardD(d);
+            return result;
+        }
+
+    }
+}
