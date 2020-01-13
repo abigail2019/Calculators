5b5701760e08b1aca4fbc482c422185c4fe02788
abigail <abigailnankamba@gmail.com>
2020-01-13 10:34:47 +0200

    Task: Created a Variance

---
+++ Variances/Variancesc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using StaticMean;
+using StatOperations;
+namespace Variances
+{
+    public interface IVariancesc
+    {
+        public dynamic Variance(dynamic array);
+    }
+    public class Variancesc: IVariancesc
+    {
+        public dynamic result;
+        public dynamic Variance(dynamic array)
+        {
+            result = StatOperations.StatOperations.Variance(array);
+            return result;
+        }
+
+    }
+}
