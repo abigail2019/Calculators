801d208a8d761117a021c15c9ed69d1f5bb6a609
abigail <abigailnankamba@gmail.com>
2020-01-13 11:55:05 +0200

    update for: stat Calculator, and calculator.sln

--- StatisticsCalculator/StatCalculator.cs
+++ StatisticsCalculator/StatCalculator.cs
@@ -2,15 +2,35 @@
 using StatOperations;
 using Calculator04;
 using StaticMean;
+using Variances;
+using StandardDeviation;
 namespace StatisticsCalculator
 {
-    public class StatCalculator : Calculator, IStatMean
+    
+    public class StatCalculator : Calculator, IStatMean, IVariancesc, IStandardDiv
     {
-        StatMean mean = new StatMean();
-        public dynamic Mean(dynamic values)
-        {
-            return mean.Mean(values);
-    }
+        //public dynamic result1;
+        readonly StatMean mean = new StatMean();
+        readonly Variancesc vari = new Variancesc();
+        readonly StandardDiv sd = new StandardDiv();
 
+        public dynamic Mean(dynamic array)
+        {
+            //result = StatOperations.StatOperations.Mean(array);
+           result = mean.Mean(array);
+           return result;
+        }
+        public dynamic Variance(dynamic array)
+        {
+            //result = StatOperations.StatOperations.Mean(array);
+            result = vari.Variance(array);
+            return result;
+        }
+        public dynamic StandardD(dynamic d)
+        {
+            //result = StatOperations.StatOperations.Mean(array);
+            result = sd.StandardD(d);
+            return result;
+        }
     }
 }
